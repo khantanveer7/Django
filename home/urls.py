f15edeff18bc0fe056454ea2e56f999e6847e40b
KHAN TANVEER <57847199+khantanveer7@users.noreply.github.com>
2020-06-27 18:58:41 +0530

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import include, path
+from home import views
+
+#Django Customization 
+admin.site.site_header = "Encode Programming"
+admin.site.site_title = "Encode Programming"
+admin.site.index_title = "Welcome to Dashboard"
+
+
+
+urlpatterns = [
+    path("", views.home, name = 'Home'),
+    path("about", views.about, name = 'About'),
+    path("contact", views.contact, name = 'Contact'),
+    path("search", views.search, name = 'Search'),
+    path("blog", views.blog, name = 'Blog'),
+    path("post/<str:slug>", views.post, name = 'Post'),
+
+]
---
+++ home/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render 
+from django.http import HttpResponse , request
+from home.models import Contact
+from home.models import Blog
+
+
+
+# Create your views here.
+
+# def home(request):
+#     return render(request,'index.htm' )
+
+def home(request):
+    # return HttpResponse("THIS IS FIRST WEB ON DJANGO")
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        desc = request.POST['desc']
+       # print(name , email , desc)
+        contact = Contact(name = name , email = email, desc = desc)
+        contact.save()
+    return render(request, 'contact.html')
+
+def blog(request):
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog.html' , context)
+
+def post(request , slug):
+    blogs = Blog.objects.filter(slug = slug).first()
+    context = {'blogs': blogs}
+    return render(request, 'post.html' , context)
+
+def search(request):
+    return render(request, 'search.html')
+
